d788158a049f4f6f3688d64ce37e50b518f5abb8
Caio <caiodib10@gmail.com>
2024-06-20 14:20:16 -0300

    Adição do gráfico de linha no caso de 1000 elementos
    Adição do gráfico de linha no caso de 1000 elementos

--- app.py
+++ app.py
@@ -6,13 +6,12 @@ import plotly.express as px
 df = pd.read_csv('algoritmos.csv')
 
 # Título principal
-st.title('Análise de Algoritmos')
+st.title('Análise de Algoritmos - Estrutura de Dados 2')
 
 # Exibir o DataFrame completo
 st.write(df)
 st.markdown("<hr>", unsafe_allow_html=True)
 
-# Convertendo as colunas de desempenho para tipos numéricos
 df['Melhor'] = df['Melhor'].astype(str).str.replace('.', '').str.replace(',', '.').astype(float)
 df['Médio'] = df['Médio'].astype(str).str.replace('.', '').str.replace(',', '.').astype(float)
 df['Pior'] = df['Pior'].astype(str).str.replace('.', '').str.replace(',', '.').astype(float)
@@ -28,21 +27,30 @@ df_filtrado = df[df['Elementos'] == num_elementos]
 
 # Gráfico de Desempenho Melhor
 st.title(f'Desempenho Melhor para {num_elementos} elementos')
-fig_melhor = px.bar(df_filtrado, x='Nome do algoritmo', y='Melhor',color='Melhor')
-fig_melhor.update_layout(xaxis_tickangle=-45, yaxis_title='Microssegundos')
+#Para exibit o gráfico de linha em caso todos elementos forem 0, para uma melhor visu
+if df_filtrado['Melhor'].max() == 0:
+    # Se todos os valores forem 0, gera um gráfico de linha
+    fig_melhor = px.line(df_filtrado, x='Nome do algoritmo', y='Melhor')
+    fig_melhor.update_layout(xaxis_tickangle=-45, yaxis_title='Microssegundos', yaxis_range=[-1,1])  
+else:
+    # Se houver valores diferentes de 0, gera um gráfico de barras
+    fig_melhor = px.bar(df_filtrado, x='Nome do algoritmo', y='Melhor', color='Melhor', color_continuous_scale=["#FFFFFF", "#00FF00"])
+    fig_melhor.update_layout(xaxis_tickangle=-45, yaxis_title='Microssegundos')
+
 st.plotly_chart(fig_melhor)
 st.markdown("<hr>", unsafe_allow_html=True)
 
+
 # Gráfico de Desempenho Médio
 st.title(f'Desempenho Médio para {num_elementos} elementos')
-fig_media = px.bar(df_filtrado, x='Nome do algoritmo', y='Médio',color = 'Médio')
+fig_media = px.bar(df_filtrado, x='Nome do algoritmo', y='Médio', color='Médio', color_continuous_scale=["#FFFFFF", "#00FF00"])
 fig_media.update_layout(xaxis_tickangle=-45, yaxis_title='Microssegundos')
 st.plotly_chart(fig_media)
 st.markdown("<hr>", unsafe_allow_html=True)
 
 # Gráfico de Pior Desempenho
 st.title(f'Pior Desempenho para {num_elementos} elementos')
-fig_pior = px.bar(df_filtrado, x='Nome do algoritmo', y='Pior',color='Pior')
+fig_pior = px.bar(df_filtrado, x='Nome do algoritmo', y='Pior',color='Pior', color_continuous_scale=["#FFFFFF", "#00FF00"])
 fig_pior.update_layout(xaxis_tickangle=-45, yaxis_title='Microssegundos')
 st.plotly_chart(fig_pior)
 st.markdown("<hr>", unsafe_allow_html=True)
